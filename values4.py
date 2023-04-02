64f1ce39a733d1943a1fc7c3070d712e94fe0f91
Alexey Razumovsky <razumyanchik@mail.ru>
2023-04-02 03:13:36 +0300

    Create values4.py

---
+++ values4.py
@@ -0,0 +1,47 @@
+1
+# Задача № 24 (Литкод) об уродливых номерах
+minimum - smallest_number
+
+2
+# Задача № 1 "TwoSum" (литкод)
+current - current_item
+
+3
+# Задача № 2 "addTwoNumbers" (Литкод)
+v_ume - memorized
+
+4
+# Задача № 3 "length Of Longest Substring" (Литкод)
+current - current_string
+
+5
+# Задача № 5 "Longest Palindromic Substring" (Литкод)
+max_len - length_max
+
+6
+# Задача № 7 "Reverse Integer" (Литкод)
+res - reversed_number
+
+7
+# Задача № 11 "Container With Most Water" (Литкод)
+volume - tank_volume
+
+8
+# Задача № 15 "3 Sum" (Литкод)
+l, r - left, right      # указатели в списке
+
+9
+# Задача № 17 "Letter Combinations of a Phone Number" (литкод)
+Words - prefix  # уже раскодированная часть текста (в рекурсивной функции)
+
+10
+# Задача № 29 "Divide" (Литкод)
+ostatok - remainder
+
+11
+# Задача № 34 "Find first and last position" (Литкод)
+first - first_position
+
+12
+# Задача № 53 "Maximum subarray" (Литкод)
+temp - temporary_amount
