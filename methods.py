1d9ea1de09531b836cbdb1532da0d49c12758ed2
Alexey Razumovsky <razumyanchik@mail.ru>
2023-04-04 23:46:51 +0300

    Create methods.py

---
+++ methods.py
@@ -0,0 +1,55 @@
+3.1
+# Улучшите пять имён классов в вашем коде.
+
+1
+# Вью-класс регистрации нового пользователя в веб-приложении
+class RegiserUser - class Registration
+
+2
+# Царство животных в классификации живых организмов
+class Animals - class Animal
+
+3
+# Класс птиц в классификации животного мира
+class Birds - class Bird
+
+4
+# Класс - группа автомобилей
+class Roadsters - class Roadster
+
+5
+# Описание полей в модели профайла пользователя
+class UserData - class User
+
+
+
+3.2
+# Улучшите семь имён методов и объектов по схеме из пункта 2.
+
+1
+# вью-функция которая показывает детализацию обной публикации (по ее ID)
+def post_detail(request, post_id) - def get_post_detail(request, post_id)
+
+2
+# Функция которая демонстрирует возведение в квадрат 
+def f(x) - def square(x)
+
+3
+# Функция в калькуляторе
+def calculate_numbers(number1, number2) - def divide(number1, number2)
+
+4
+# Функция в калькуляторе
+def calculate_1_numbers(number1, number2) - def multiply(number1, number2)
+
+5
+# Метод в игровом движке
+def screen_updater(self) - def update_screen(self)
+
+6
+# Метод который отрисовывает изображение корабля в игре
+def sheep_place(self) - def blitme(self) 
+
+7
+# Увеличение скорости персонажей игры с повышением уровня
+def speeding_up(self) - def increase_speed(self)
